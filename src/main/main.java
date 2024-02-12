57dcf885cbcdfd234845edefcebccf7bb8284238
Zahdi <130311308+MohcineProject@users.noreply.github.com>
2024-02-12 23:04:15 +0100

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -11,12 +11,9 @@ public class main {
 
 	public static void main(String[] args)  {
 		FrameIMSAT fi = new FrameIMSAT();
-        // Set your preferred size (you can adjust this according to your needs)
         int width = 800;
         int height = 600;
         fi.setSize(width, height);
-
-        // Calculate the center of the screen
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (screenSize.width - width) / 2;
         int y = (screenSize.height - height)/2;
